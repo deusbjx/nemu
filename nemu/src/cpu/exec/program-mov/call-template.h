0769bb7da4c89f0c4ffd91d1a5093c1082dccf23
tracer-ics2016 <tracer@njuics.org>
2020-09-17 16:27:37 +0000

    >  compile NEMU 3018216032 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:27:37 up 40 min,  1 user,  load average: 0.06, 0.15, 0.18 6fe501c674862c67fc55681f47401bb1661e652

--- nemu/src/cpu/exec/program-mov/call-template.h
+++ nemu/src/cpu/exec/program-mov/call-template.h
@@ -4,10 +4,10 @@
 
 make_helper(concat(call_i_, SUFFIX)) {
 	DATA_TYPE_S value = op_src->val;
-	uint32_t length = concat(decode_i_,SUFFIX)(eip+1);
+	int length = concat(decode_i_,SUFFIX)(eip+1);
 	reg_l(R_ESP) -= DATA_BYTE;
 	swaddr_write(reg_l(R_ESP),4,cpu.eip+length);
-	print_asm("call %x",cpu.eip+1+value+length);
+	print_asm("call1 $0x%x",cpu.eip+1+value+length);
 	cpu.eip+=value;
 	return length+1;
 }
@@ -17,7 +17,7 @@ make_helper(concat(call_rm_, SUFFIX)) {
 	uint32_t length = concat(decode_rm_,SUFFIX)(eip+1);
 	reg_l(R_ESP) -= DATA_BYTE;
 	swaddr_write(reg_l(R_ESP),4,cpu.eip+length);
-	print_asm("call %x",value);
+	print_asm("call2 $0x%x",value);
 	cpu.eip=value-length-1;
 	return length+1;
 }
