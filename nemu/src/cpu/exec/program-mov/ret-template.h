fcc734a8c11536ad9e6542852424f72f2ed3be33
tracer-ics2016 <tracer@njuics.org>
2020-09-17 16:05:07 +0000

    >  compile NEMU 3018216032 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:05:07 up 17 min,  1 user,  load average: 0.16, 0.21, 0.20 d258727e89750ea4a6bc7a14407692bf77323f7a

--- nemu/src/cpu/exec/program-mov/ret-template.h
+++ nemu/src/cpu/exec/program-mov/ret-template.h
@@ -14,7 +14,7 @@ make_helper(concat(ret_i_, SUFFIX)) {
 	if (DATA_BYTE == 2){
 		cpu.eip &= 0xffff;
 	}
-	MEM_W(REG(R_ESP)+i,0);
+	//MEM_W(REG(R_ESP)+i,0);
 	REG(R_ESP)+=value;
 	print_asm("ret 0x%x",value);
 	return 0;
