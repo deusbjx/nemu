3e3a838863724db684c7f34020240aebf117341b
tracer-ics2016 <tracer@njuics.org>
2020-09-18 01:09:51 +0000

    >  compile NEMU 3018216032 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:09:51 up  9:22,  1 user,  load average: 0.29, 0.10, 0.03 d4dfc9d025ab2fc3b6c1156be55913b83d516403

--- nemu/src/cpu/exec/program-mov/call-template.h
+++ nemu/src/cpu/exec/program-mov/call-template.h
@@ -8,7 +8,7 @@ make_helper(concat(call_i_, SUFFIX)) {
 	reg_l(R_ESP) -= DATA_BYTE;
 	swaddr_write(reg_l(R_ESP),4,cpu.eip+length);
 	print_asm("call1 $0x%x",cpu.eip+1+length+value);
-	cpu.eip+=11;
+	cpu.eip+=length;
 	//print_asm("eip $0x%x,%d",cpu.eip,length);
 	return length+1;
 }
