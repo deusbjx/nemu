b1309dd4269aae37d96bef4bfd329633055291fa
tracer-ics2016 <tracer@njuics.org>
2020-09-24 02:00:50 +0000

    >  compile NEMU 3018216032 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:00:50 up 13 min,  1 user,  load average: 0.21, 0.34, 0.30 28002c15b2c7115937ec682b2f51b199b31b52fe

--- nemu/src/cpu/exec/control-trans/ret-template.h
+++ nemu/src/cpu/exec/control-trans/ret-template.h
@@ -16,13 +16,13 @@ make_helper(concat(ret_i_, SUFFIX)) {
 	int value = instr_fetch(eip+1,2);
 	//current_sreg = R_SS;
 	cpu.eip = MEM_R(REG(R_ESP));
+	if (DATA_BYTE == 2){
+		cpu.eip &= 0xffff;
+	}
 	REG(R_ESP)+=DATA_BYTE;
 	int i;
 	for (i = 0;i < value;i+=DATA_BYTE){
 		MEM_W(REG(R_ESP)+i,0);
-	}
-	if (DATA_BYTE == 2){
-		cpu.eip &= 0xffff;
 	}
 	//MEM_W(REG(R_ESP)+i,0);
 	REG(R_ESP)+=value;
