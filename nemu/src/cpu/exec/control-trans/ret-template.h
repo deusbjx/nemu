86f4303f69633ef2b8fe1cf6d40f900c5a22bd29
tracer-ics2016 <tracer@njuics.org>
2020-09-24 15:54:18 +0000

    >  compile NEMU 3018216032 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:54:18 up 9 min,  1 user,  load average: 0.31, 0.44, 0.27 1648312369e806be25c801361e3b19994fedc7e

--- nemu/src/cpu/exec/control-trans/ret-template.h
+++ nemu/src/cpu/exec/control-trans/ret-template.h
@@ -8,26 +8,28 @@ make_helper(concat(ret_n_, SUFFIX)) {
 	cpu.eip = MEM_R(reg_l(R_ESP));
 	reg_l(R_ESP)+=DATA_BYTE;
 	//MEM_W(REG(R_ESP)+i,0);
-	print_asm("ret 0x%x",cpu.eip);
+	print_asm("ret");
 	return 1;
 }
 
 make_helper(concat(ret_i_, SUFFIX)) {
 	int value = instr_fetch(eip+1,2);
 	//current_sreg = R_SS;
-	cpu.eip = MEM_R(REG(R_ESP));
+	cpu.eip = MEM_R(REG(R_ESP))-3;
 	if (DATA_BYTE == 2){
 		cpu.eip &= 0xffff;
 	}
-	REG(R_ESP)+=DATA_BYTE;
+	/*REG(R_ESP)+=DATA_BYTE;
 	int i;
 	for (i = 0;i < value;i+=DATA_BYTE){
 		MEM_W(REG(R_ESP)+i,0);
 	}
 	//MEM_W(REG(R_ESP)+i,0);
-	REG(R_ESP)+=value;
+	REG(R_ESP)+=value;*/
+	reg_l(R_ESP) += DATA_BYTE;
+	reg_l(R_ESP) += value;
 	print_asm("ret 0x%x",value);
-	return 1;
+	return 3;
 }
 
 
