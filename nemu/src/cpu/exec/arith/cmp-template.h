17253bbc25f86c3a7a3aac3d64742b4a01ed5b3f
tracer-ics2016 <tracer@njuics.org>
2020-09-24 01:10:28 +0000

    >  run 3018216032 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:10:28 up 3 min,  1 user,  load average: 0.45, 0.39, 0.17 cf2c600736239b787b658632ae940b11a8460ce8

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -29,7 +29,7 @@ make_instr_helper(si2rm)
 #endif
 
 make_instr_helper(i2a)
-make_instr_helper(i2r)
+//make_instr_helper(i2r)
 make_instr_helper(i2rm)
 make_instr_helper(r2rm)
 make_instr_helper(rm2r)
