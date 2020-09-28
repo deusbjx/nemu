f793296e3d03335a3b29f3b6476d28a055026c08
tracer-ics2016 <tracer@njuics.org>
2020-09-28 11:45:21 +0000

    >  compile NEMU 3018216032 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:45:21 up  8:10,  1 user,  load average: 0.39, 0.39, 0.43 a2a126fb59beb3edeef290b12c184a2e37237ced

--- nemu/src/cpu/exec/string/movs.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MOVS_H__
-#define __MOVS_H__
-
-make_helper(movs_i_b);
-
-make_helper(movs_i_v);
-
-#endif
--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -5,15 +5,17 @@
 make_helper(concat(scas_, SUFFIX)) {
 	swaddr_t se = REG(R_EAX);
 	swaddr_t sd = swaddr_read(reg_l(R_EDI), DATA_BYTE);
+	int length = (DATA_BYTE << 3) - 1;
 	int ans = se - sd;
 	cpu.CF = se < sd;
+	cpu.ZF = !ans;
 	if (cpu.DF == 0) reg_l(R_EDI) += DATA_BYTE;
 	else reg_l(R_EDI) -= DATA_BYTE;
+	cpu.SF = ans >> length;
 	ans ^= ans >> 4;
 	ans ^= ans >> 2;
 	ans ^= ans >> 1;
 	cpu.PF = !(ans & 0x1);
-	int length = (DATA_BYTE << 3) - 1;
 	int s1 = se >> length;
 	int s2 = sd >> length;
     	cpu.OF = ((s1 != s2) && (s2 == cpu.SF));
