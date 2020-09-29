073f389b9ca02b9e3905335eaf6ba73d60baf8de
tracer-ics2016 <tracer@njuics.org>
2020-09-29 16:46:20 +0000

    >  compile NEMU 3018216032 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:46:20 up  8:49,  1 user,  load average: 0.54, 0.56, 0.54 56f75c848b2880fd36183d02142000b64e0868a

--- nemu/src/cpu/exec/control-trans/jl-template.h
+++ nemu/src/cpu/exec/control-trans/jl-template.h
@@ -5,7 +5,7 @@
 static void do_execute() {
 	DATA_TYPE_S value = op_src->val;	
 	print_asm("jl %x",cpu.eip+1+DATA_BYTE+value);
-	if (cpu.ZF == 0 && cpu.SF != cpu.OF)cpu.eip+=value;
+	if (cpu.SF != cpu.OF)cpu.eip+=value;
 }
 
 make_instr_helper(i)
