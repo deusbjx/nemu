cc9a1026525e9d1313943c4904370b4e7923a959
tracer-ics2016 <tracer@njuics.org>
2020-09-29 16:17:58 +0000

    >  run 3018216032 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:17:58 up  8:21,  1 user,  load average: 0.28, 0.21, 0.09 d9f94878aa98a66dd761f5dc19cf23ffae2bbe

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -6,14 +6,14 @@ FLOAT f(FLOAT x) {
 	return F_div_F(int2F(1), int2F(1) + F_mul_int(F_mul_F(x, x), 25));
 }
 
-FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
+FLOAT computeT(int n, FLOAT a, FLOAT b) {
 	int k;
 	FLOAT s,h;
 	h = F_div_int((b - a), n);
-	//s = F_div_int(fun(a) + fun(b), 2 );
-	s = F_div_int(a + b, 2 );
+	s = F_div_int(f(a) + f(b), 2 );
+	//s = F_div_int(a + b, 2 );
 	for(k = 1; k < n; k ++) {
-		//s += fun(a + F_mul_int(h, k));
+		s += f(a + F_mul_int(h, k));
 		s += a + F_mul_int(h, k);
 	}
 	s = F_mul_F(s, h);
@@ -28,7 +28,7 @@ int main() {
 	//FLOAT ans = F_mul_F(f(a),f(ans2));
 	//nemu_assert(ans);
 
-	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
+	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0));
 	FLOAT ans = f2F(0.551222);
 
 	nemu_assert(Fabs(a - ans) < f2F(1e-4));
