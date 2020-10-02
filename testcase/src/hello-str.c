0b7046a07b2d185023541f6a26b35a62f5b47c34
tracer-ics2016 <tracer@njuics.org>
2020-10-02 06:48:53 +0000

    >  run 3018216032 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:48:53 up 14 min,  1 user,  load average: 0.16, 0.20, 0.17 fe13713bda0213b5bf2fd12eb506f4c66758fe88

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -8,15 +8,15 @@ int main() {
 	
 	set_bp();
 	sprintf(buf, "%s", "Hello world!\n");
-	set_bp();
+	//set_bp();
 	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
-	set_bp();
+	//set_bp();
 
 	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
 	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
 
-	set_bp();
+	//set_bp();
 
 	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
 	nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
