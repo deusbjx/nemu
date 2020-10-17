a3671a790f0ad3c323e7cf19f13d2c4505153fc9
tracer-ics2016 <tracer@njuics.org>
2020-10-17 16:19:24 +0000

    >  compile NEMU 3018216032 simpleedu Linux simpleedu 4.15.0-121-generic #123-Ubuntu SMP Mon Oct 5 16:16:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:19:24 up  7:26,  1 user,  load average: 0.56, 0.69, 0.63 3a3e620ddaa4d81427e1f6be68d74e8c9f4a5a94

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -72,7 +72,7 @@ void cache_write(hwaddr_t addr, size_t len,uint32_t data) {
 				dram_write(addr, CACHE_BLOCK_SIZE - in_addr, data);	//write through
 				memcpy(cache[i].data + in_addr, &data, len);
 				//cache2_write(addr, len, data);//update cache2
-				cache_write(addr + len, len - CACHE_BLOCK_SIZE + in_addr, data >> len);
+				cache_write(addr + len, len - CACHE_BLOCK_SIZE + in_addr, data >> (CACHE_BLOCK_SIZE - in_addr));
 			} 
 			else {
 				dram_write(addr, len, data);
