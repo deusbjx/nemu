06351a7deea6ffa300d5f1e13902a4bee4c3de43
tracer-ics2016 <tracer@njuics.org>
2020-10-17 16:47:50 +0000

    >  compile NEMU 3018216032 simpleedu Linux simpleedu 4.15.0-121-generic #123-Ubuntu SMP Mon Oct 5 16:16:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:47:50 up  7:55,  1 user,  load average: 0.91, 0.80, 0.74 3b4bd1ddd6cdb7374db55c7df5aad3fb6ac05dd3

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -30,7 +30,7 @@ int cache_read(hwaddr_t addr)
 {
 	uint32_t g_num = (addr >> 6) & 0x7f; //group number
 	printf("group number:%d\n",(g_num+1)*WAY_8);
-	uint32_t block = (addr >> 6)<<6;
+	//uint32_t block = (addr >> 6)<<6;
 	int i,j;
 	bool flag = false;
 	for (i = g_num * WAY_8 ; i < (g_num + 1) * WAY_8 ;i++){
@@ -40,7 +40,7 @@ int cache_read(hwaddr_t addr)
 		}
 	}
 	if (flag == false) {
-		//int j = cache2_read (addr);
+		j = cache2_read (addr);
 		for (i = g_num * WAY_8 ; i < (g_num+1) * WAY_8 ;i++){
 			if (!cache[i].valid)break;
 		}
@@ -49,14 +49,15 @@ int cache_read(hwaddr_t addr)
 			srand (0);
 			i = g_num * WAY_8 + rand() % WAY_8;
 		}
+		memcpy (cache[i].data,cache2[j].data,CACHE_BLOCK_SIZE);
 		//srand(i);
 		//i = WAY_8 * g_num + rand() % WAY_8;//random
-		for(j = 0; j < CACHE_BLOCK_SIZE / BURST_LEN; j++) {
+		/*for(j = 0; j < CACHE_BLOCK_SIZE / BURST_LEN; j++) {
 			ddr3_read_public(block + j * BURST_LEN , cache[i].data + j * BURST_LEN);
-		}
+		}*/
 		cache[i].valid = true;
 		cache[i].tag = addr >> 13;
-		//memcpy (cache[i].data,cache2[j].data,CACHE_BLOCK_SIZE);
+		
 	}
 	return i;
 }
@@ -71,19 +72,19 @@ void cache_write(hwaddr_t addr, size_t len,uint32_t data) {
 			if(in_addr + len > CACHE_BLOCK_SIZE) {//across
 				dram_write(addr, CACHE_BLOCK_SIZE - in_addr, data);	//write through
 				memcpy(cache[i].data + in_addr, &data, len);
-				//cache2_write(addr, len, data);//update cache2
+				cache2_write(addr, CACHE_BLOCK_SIZE - in_addr, data);//update cache2
 				cache_write(addr + len, len - CACHE_BLOCK_SIZE + in_addr, data >> (CACHE_BLOCK_SIZE - in_addr));
 			} 
 			else {
 				dram_write(addr, len, data);
 				memcpy(cache[i].data + in_addr, &data, len);
-				//cache2_write(addr, len, data);
+				cache2_write(addr, len, data);
 			}
 			return;
 		}
 		//else dram_write(addr, len, data);
 	}
-	dram_write(addr, len, data);
+	//dram_write(addr, len, data);
 	//dram_write(addr,len,data);
 	/*for (i = g_num * WAY_8 ; i < (g_num + 1) * WAY_8 ;i++)
 	{
@@ -98,9 +99,10 @@ void cache_write(hwaddr_t addr, size_t len,uint32_t data) {
 		memcpy (cache[i].data + in_addr , &data , len);
 	}
 	cache2_write(addr,len,data);*/
-	i = cache_read(addr);
-	cache[i].valid = true;
-	memcpy(cache[i].data + in_addr, &data, len);
+	cache2_write(addr,len,data);
+	//i = cache_read(addr);
+	//cache[i].valid = true;
+	//memcpy(cache[i].data + in_addr, &data, len);
 }
 
 int cache2_read(hwaddr_t addr) {
@@ -123,13 +125,14 @@ int cache2_read(hwaddr_t addr) {
 		if (i == (g_num + 1) * WAY_16){
 			srand (0);
 			i = g_num * WAY_16 + rand() % WAY_16;
-			if (cache2[i].dirty) {
+		}
+		if (cache2[i].dirty && cache2[i].valid) {
 				uint8_t mask[BURST_LEN * 2];
 				memset(mask, 1, BURST_LEN * 2);
+				uint32_t block2 = (cache2[i].tag << 18) | (g_num << 6);
 				for (j = 0;j < CACHE2_BLOCK_SIZE/BURST_LEN;j++){
-					ddr3_write_public(block + j * BURST_LEN, cache2[i].data + j * BURST_LEN, mask);
+					ddr3_write_public(block2 + j * BURST_LEN, cache2[i].data + j * BURST_LEN, mask);
 				}
-			}
 		}
 		cache2[i].valid = true;
 		cache2[i].tag = addr >> 18;
@@ -153,8 +156,20 @@ void cache2_write(hwaddr_t addr, size_t len, uint32_t data) {
 				break;
 		}
 	}
-	if (!flag)i = cache2_read (addr);
+	if (flag){
+		cache2[i].dirty = true;
+		if(in_addr + len > CACHE2_BLOCK_SIZE) {//across
+			//dram_write(addr, CACHE2_BLOCK_SIZE - offset, data);	//write through
+			memcpy(cache2[i].data + in_addr, &data, CACHE2_BLOCK_SIZE - in_addr);
+			cache2_write(addr + CACHE2_BLOCK_SIZE - in_addr, len - CACHE2_BLOCK_SIZE + in_addr, data >> (CACHE2_BLOCK_SIZE - in_addr));
+		} else {
+			//dram_write(addr, len, data);	//write through
+			memcpy(cache2[i].data + in_addr, &data, len);
+		}
+		return;
+	}
+	i = cache2_read(addr);
 	cache2[i].dirty = true;
-	memcpy (cache2[i].data + in_addr , &data , len);
+	memcpy(cache2[i].data + in_addr, &data, len);
 }
 
