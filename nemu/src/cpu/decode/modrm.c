b654aa9d9364fd26fc0a97501c2b47bb18c6132d
tracer-ics2016 <tracer@njuics.org>
2020-10-19 03:46:23 +0000

    >  compile NEMU 3018216032 simpleedu Linux simpleedu 4.15.0-121-generic #123-Ubuntu SMP Mon Oct 5 16:16:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:46:23 up 18:56,  1 user,  load average: 1.02, 0.64, 0.46 18ef5715efb0d1fd43753501baa0e65fef6db48d

--- nemu/src/cpu/decode/modrm.c
+++ nemu/src/cpu/decode/modrm.c
@@ -118,7 +118,7 @@ int read_ModR_M(swaddr_t eip, Operand *rm, Operand *reg) {
 		else {
 			rm->sreg = R_DS;
 		}
-		//rm->val = swaddr_read(rm->addr, rm->size);
+		rm->val = swaddr_read(rm->addr, rm->size, rm->sreg);
 		return instr_len;
 	}
 }
--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -77,8 +77,8 @@ static void load_entry() {
 }
 
 static void init_CR0() {
-	cpu.cr0.protect_enable = 1;
-	cpu.cr0.paging = 0;
+	cpu.cr0.protect_enable = 0;
+	//cpu.cr0.paging = 0;
 	cpu.cr0.val = cpu.cr3.val = 0;
 }
 
