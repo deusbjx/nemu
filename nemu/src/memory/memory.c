ad69068bfe7852840bd9b2e885495f332dd548f2
tracer-ics2016 <tracer@njuics.org>
2020-10-19 15:53:54 +0000

    >  compile NEMU 3018216032 simpleedu Linux simpleedu 4.15.0-121-generic #123-Ubuntu SMP Mon Oct 5 16:16:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:53:54 up 1 day,  7:03,  1 user,  load average: 0.52, 0.49, 0.45 78691738a694619a341a9929b4e350cb7fcf1324

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -53,7 +53,7 @@ uint32_t swaddr_read(swaddr_t addr, size_t len, uint8_t sreg) {
 	assert(len == 1 || len == 2 || len == 4);
 #endif
 	lnaddr_t lnaddr = seg_translate(addr,len,sreg);
-	printf("addr: %d\n",lnaddr);
+	//printf("addr: %d\n",lnaddr);
 	return lnaddr_read(lnaddr, len);
 }
 
@@ -68,7 +68,7 @@ void swaddr_write(swaddr_t addr, size_t len, uint32_t data, uint8_t sreg) {
 /* seg function*/
 lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg) {
 	if (cpu.cr0.protect_enable == 1) {
-		printf("Changed!\n");
+		//printf("Changed!\n");
 		Assert(addr+len < cpu.sr[sreg].cache_limit, "Segment Fault!");
 		return cpu.sr[sreg].cache_base + addr;
 	}
