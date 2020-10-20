28d3136c4050f0e80b6b3427be447f06ff12ffad
tracer-ics2016 <tracer@njuics.org>
2020-10-20 03:42:37 +0000

    >  compile NEMU 3018216032 simpleedu Linux simpleedu 4.15.0-121-generic #123-Ubuntu SMP Mon Oct 5 16:16:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:42:37 up 34 min,  1 user,  load average: 0.79, 0.48, 0.38 d1a6120d6ca4cae893ff6e04e5fa2d11ed7958ef

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -8,6 +8,7 @@ uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 lnaddr_t seg_translate(swaddr_t, size_t, uint8_t);
 void sreg_load(uint8_t);
+hwaddr_t page_translate(lnaddr_t, size_t); 
 
 
 /* Memory accessing interfaces */
@@ -41,11 +42,15 @@ void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
-	return hwaddr_read(addr, len);
+	/*page*/
+	hwaddr_t hwaddr = page_translate(addr, len); 
+	return hwaddr_read(hwaddr, len);
 }
 
 void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
-	hwaddr_write(addr, len, data);
+	/*page*/
+	hwaddr_t hwaddr = page_translate(addr, len);
+	hwaddr_write(hwaddr, len, data);
 }
 
 uint32_t swaddr_read(swaddr_t addr, size_t len, uint8_t sreg) {
@@ -87,5 +92,27 @@ void sreg_load(uint8_t sreg) {
 	if(sd.g) limit <<= 12;
 	cpu.sr[sreg].cache_limit = limit;
 	cpu.sr[sreg].cache_base = base;
+}
+
+/*page function*/
+hwaddr_t page_translate(lnaddr_t addr, size_t len) {
+	if(cpu.cr0.protect_enable && cpu.cr0.paging) {
+		//hwaddr_t tmpad;
+		//if((tmpad = readTLB(addr & 0xfffff000)) != -1) return (tmpad << 12) + (addr & 0xfff);
+		PageDescriptor dir, page;
+		uint32_t dir_in_addr = addr >> 22;
+		uint32_t page_in_addr = ((addr >> 12) & 0x3ff);
+		uint32_t in_addr = addr & 0xfff;
+		dir.page_val = hwaddr_read((cpu.cr3.page_directory_base << 12) + (dir_in_addr << 2), 4);
+		Assert(dir.p, "Invalid page!"); //avoid error
+		page.page_val = hwaddr_read((dir.addr << 12) + (page_in_addr << 2), 4);
+		Assert(page.p, "Invalid page!"); //avoid error
+	//	hwaddr_t hwaddr = (page.base << 12) + offset;
+		//Assert((hwaddr & 0xfff) + len == ((hwaddr + len) & 0xfff), "Fatal Error!!");
+		//writeTLB(addr & 0xfffff000, page.base);
+		return (page.addr << 12) + in_addr;
+	} else {
+		return addr;
+	}
 }
 
