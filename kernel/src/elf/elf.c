7cf5c94182c57fa6b3b57ec5ee4c2c52a3f49d86
tracer-ics2016 <tracer@njuics.org>
2020-10-21 09:16:43 +0000

    >  compile kernel 3018216032 simpleedu Linux simpleedu 4.15.0-121-generic #123-Ubuntu SMP Mon Oct 5 16:16:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:16:43 up 1 day,  3:14,  1 user,  load average: 1.04, 0.97, 0.74 c083d06216efaae41adbcb6bd211b734856f3b84

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -47,13 +47,14 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
+			set_bp();
 			ramdisk_read((void *)ph->p_vaddr, ph->p_offset, ph->p_filesz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
 			memset((void *)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
-			set_bp();
+			
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
