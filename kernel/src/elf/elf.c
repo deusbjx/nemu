530f6d3cdff0947aa7220b71f42985b5509bcf24
tracer-ics2016 <tracer@njuics.org>
2020-10-21 08:57:43 +0000

    >  compile kernel 3018216032 simpleedu Linux simpleedu 4.15.0-121-generic #123-Ubuntu SMP Mon Oct 5 16:16:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:57:43 up 1 day,  2:55,  1 user,  load average: 1.22, 0.52, 0.25 14a069668a733cc8c9fcee6220e974e4aa7627d0

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -41,7 +41,7 @@ uint32_t loader() {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
-                        uint32_t pa = ph->p_vaddr;
+                        uint32_t pa = mm_malloc(ph->p_vaddr,ph->p_memsz);
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
