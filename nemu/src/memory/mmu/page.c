c95423501271483d97ca563bac9a41b3ce1fc8f3
tracer-ics2016 <tracer@njuics.org>
2020-11-20 07:42:32 +0000

    >  compile NEMU 3018216032 simpleedu Linux simpleedu 4.15.0-123-generic #126-Ubuntu SMP Wed Oct 21 09:40:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:42:32 up  2:41,  1 user,  load average: 0.62, 0.58, 0.48 6db567612991f794d1ceb569a406f768de09d30f

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -22,22 +22,22 @@ hwaddr_t page_walk(lnaddr_t lnaddr) {
 	pde.val	= hwaddr_read(pdir_base + addr->pdir_idx * 4, 4);
 	//set_bp();
 	//Log("test");
-	/*
+	
 	if(!pde.present) {
 		Log("eip = %x, lnaddr = %x, pdir_base = %x, pde = %x", cpu.eip, lnaddr, pdir_base, pde.val);
 		Log("Error1!");
 		assert(0);
-	}*/
+	}
 
 	hwaddr_t pt_base = pde.val & ~PAGE_MASK;
 	PTE pte;
 	pte.val = hwaddr_read(pt_base + addr->pt_idx * 4, 4);
-	/*
+	
 	if(!pte.present) {
 		Log("eip = %x, lnaddr = %x, pt_base = %x, pte = %x", cpu.eip, lnaddr, pt_base, pte.val);
 		Log("Error2!");
 		assert(0);
-	}*/
+	}
 	return pte.val;
 }
 
