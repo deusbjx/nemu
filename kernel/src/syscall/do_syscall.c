1572548c23dd5e30c767adce745b1a4604f079ea
tracer-ics2016 <tracer@njuics.org>
2020-12-28 11:55:56 +0000

    >  compile game 3018216032 simpleedu Linux simpleedu 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:55:56 up 47 min,  1 user,  load average: 0.51, 0.75, 0.75 a17b55f2e72320312875ff616bb8fd2398fcb22

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -18,7 +18,7 @@ static void sys_write(TrapFrame *tf) {
 	if(tf->ebx == 1 || tf->ebx == 2) { //check fd
 	#ifdef HAS_DEVICE
 		void serial_printc(char ch);
-		int i;
+		int i = 0;
 		for(i = 0; i < tf->edx; i++) serial_printc(*((char *)((char *)tf->ecx + i)));
 	#else
 		asm volatile(".byte 0xd6" : : "a"(2), "c"(tf->ecx), "d"(tf->edx));
